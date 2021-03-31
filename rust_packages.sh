fc18f28f942f81be489ba9fb85e9695ccfd00c07
Royon, Yvan <yvan.royon@verizon.com>
2021-03-30 17:52:12 -0700

    Update list of rust packages

--- rust_packages.sh
+++ rust_packages.sh
@@ -7,7 +7,7 @@ fd-find
 mdcat
 ripgrep
 )
-# antr funzzy watchexec
+# ht -> curl replacement
 
 tui=(
 bandwhich
@@ -15,18 +15,27 @@ broot
 gping
 procs
 )
-# kmon
 
 misc=(
 git-delta
 hexyl
 hyperfine
+jsonschema
 loc
 tokei
+)
+# watchexec -> notify wrapper
+
+# If not on crates.io...
+additional_repos=(
+https://github.com/jez/as-tree
 )
 
 rustup update
 
+for repo in "${additional_repos[@]}"; do
+    cargo install --force --git "$repo"
+done
 cargo install "${cli[@]}" "${tui[@]}" "${misc[@]}"
 cargo install cargo-update
 cargo install-update --all
