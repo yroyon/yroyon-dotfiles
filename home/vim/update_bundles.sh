13d08cfcdf78afec1f12d97b0c1c046fdd9446cf
Yvan Royon <>
2022-08-26 10:48:35 -0700

    Update vim plugins, and update script

--- home/vim/update_bundles.sh
+++ home/vim/update_bundles.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "$here" &> /dev/null
+
+## Initial setup, do this manually for a new git clone / new machine:
 #git submodule init
 #git submodule update
+
+## pull method, I found it unreliable (conflicts):
 #git submodule foreach git checkout master
 #git submodule foreach git pull
+
+## rebase method, preferred:
 git submodule foreach git fetch
 git submodule foreach git rebase
 
-pushd bundle/coc/
-yarn install --frozen-lockfile
-popd
+## The CoC plugin needs a heavyweight update mechanism:
+[[ -d bundle/coc/ ]] && {
+    pushd bundle/coc/ &> /dev/null
+    yarn install --frozen-lockfile
+    popd &> /dev/null
+}
